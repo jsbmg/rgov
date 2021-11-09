f712e1ed241d0fcf06a30f9313c219c698236e57
Jordan Sweet <hello@jordandsweet.com>
2021-11-09 11:38:36 -0800

    Organize variables in to classes by type.

--- rgov/utils/constants.py
+++ rgov/utils/constants.py
@@ -2,17 +2,23 @@ from os.path import join
 from pathlib import Path
 
 
-def get_project_root() -> Path:
-    return Path(__file__).parent.parent
+class Urls():
+    base_url = "https://www.recreation.gov/api/camps/availability/campground/"
+    browser_base_url = "https://www.recreation.gov/camping/campgrounds/"
+    download_url = "https://ridb.recreation.gov/downloads/RIDBFullExport_V1_CSV.zip"
 
-project_root = get_project_root()
-download_url = "https://ridb.recreation.gov/downloads/RIDBFullExport_V1_CSV.zip"
-download_folder = join(project_root, "assets")
-download_path = join(download_folder, "RIDBExport.zip")
-facilities_csv_path = join(download_folder, "Facilities_API_v1.csv")
-data_folder = join(project_root, "data")
-index_path = join(data_folder, "campgrounds_index.csv")
-base_url = "https://www.recreation.gov/api/camps/availability/campground/"
-browser_base_url = "https://www.recreation.gov/camping/campgrounds/"
-request_time_format = "%Y-%m-%dT00:00:00.000Z"
-response_time_format = "%Y-%m-%dT00:00:00Z"
+class Paths():
+    def get_project_root() -> Path:
+        return Path(__file__).parent.parent
+    
+    project_root = get_project_root()    
+    download_folder = join(project_root, "assets")
+    download_path = join(download_folder, "RIDBExport.zip")
+    facilities_csv_path = join(download_folder, "Facilities_API_v1.csv")
+    data_folder = join(project_root, "data")
+    index_path = join(data_folder, "campgrounds_index.csv")
+    log_file = join(Path.home(), ".rgov.log")
+
+class Time():
+    request_time_format = "%Y-%m-%dT00:00:00.000Z"
+    response_time_format = "%Y-%m-%dT00:00:00Z"
