33752e7b4787405b117b38215f3249428bc6b10d
Jordan Sweet <hello@jordandsweet.com>
2021-11-15 23:09:03 -0800

    Changed constants.py to locations.py and added auth_file

---
+++ rgov/locations.py
@@ -0,0 +1,20 @@
+import os 
+
+from pathlib import Path
+
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+PROJECT_ROOT = get_project_root()    
+
+DOWNLOAD_FOLDER = os.path.join(PROJECT_ROOT, "assets")
+DOWNLOAD_PATH = os.path.join(DOWNLOAD_FOLDER, "RIDBExport.zip")
+FACILITIES_CSV_PATH = os.path.join(DOWNLOAD_FOLDER, "Facilities_API_v1.csv")
+
+DATA_FOLDER = os.path.join(PROJECT_ROOT, "data")
+INDEX_PATH = os.path.join(DATA_FOLDER, "campgrounds_index.csv")
+
+CONFIG_DIR = os.getenv("XDG_CONFIG_HOME", os.path.join(Path.home(), ".config"))
+AUTH_FILE = os.path.join(CONFIG_DIR, "rgov", "auth.txt") 
+LOG_FILE = os.path.join(Path.home(), ".rgov.log")
