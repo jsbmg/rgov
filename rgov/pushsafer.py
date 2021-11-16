d63ec8588be8c510b2a015f7ec81e7acfce007de
Jordan Sweet <hello@jordandsweet.com>
2021-11-15 23:07:05 -0800

    Add functions to read and write credentials to file

--- rgov/pushsafer.py
+++ rgov/pushsafer.py
@@ -1,13 +1,38 @@
 import getpass
 import json
+import os
+from pathlib import Path
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+
+from rgov import locations
 
 def input_credentials() -> tuple[str, str]:
     username = input("Enter username/email for Pushsafer: ")
     key = getpass.getpass("Enter API key for Pushsafer: ")
     return username, key
+
+def write_credentials(email, key, passcmd=False):
+    os.makedirs(os.path.join(locations.CONFIG_DIR, "rgov"), exist_ok=True)
+    os.umask(0)
+    if passcmd:
+        print("Not yet implemented.")
+    with open(os.open(locations.AUTH_FILE, os.O_CREAT | os.O_WRONLY, 0o600), 'w') as file: 
+        file.truncate(0)
+        file.write(f"{email}\n")
+        # if passcmd:
+        #    file.write(f"passcmd: {key}")
+        # else:
+        file.write(f"{key}")
+
+def read_credentials():
+    with open(locations.AUTH_FILE, 'r') as file:
+        lines = file.readlines()
+        lines = [line.strip() for line in lines]
+        username = lines[0]
+        key = lines[1]
+        return username, key
 
 def validate_key(username: str, key: str) -> bool:
     url = 'https://www.pushsafer.com/api-k'
@@ -31,8 +56,6 @@ def gen_notifier_text(campground_results: dict) -> str:
             message += f"{name}: site(s) {available_str} available!\n"
         elif num_sites > 10:
             message += f"{name}: {num_sites} sites available!\n"
-        else:
-            continue
     return message
 
 
