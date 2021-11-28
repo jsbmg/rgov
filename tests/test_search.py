eb1d717ca85b0fab2bb4209c37775c469ffa2c71
Jordan Sweet <hello@jordandsweet.com>
2021-11-27 17:20:29 -0800

    Update search.py tests to work with new sqlite search function

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,13 +6,14 @@ def test_search():
     with pytest.raises(ValueError):
         for x in search("laguna", 1):
             pass 
-    for x in search(["laguna"], 1):
+    for x in search(["laguna"], descriptions=False):
         assert x == ('Laguna', '232279') 
     dt_search = []
-    for x in search(["Detroit"], 2):
+    for x in search(["Detroit"], descriptions=True):
         dt_search.append(x)
+
     assert dt_search == [('Breitenbush Campground', '233301'), ('Cove Creek', '233694'), ('Fox Creek Group Camp', '233209'), ('Hoover Campground', '233693'), ('Humbug Campground', '251713'), ('Olallie Lake Guard Station Cabin', '233305'), ('Riverside At Detroit (Willamette National Forest, Or)', '233258'), ('Santiam Flats Campground', '251616'), ('Southshore At Detroit Lake', '255135'), ('Whispering Falls Campground', '251470')]  
     dt_uncaps_search = []
-    for x in search(["detroit"], 2):
+    for x in search(["detroit"], descriptions=True):
         dt_uncaps_search.append(x)
     assert dt_uncaps_search == [('Breitenbush Campground', '233301'), ('Cove Creek', '233694'), ('Fox Creek Group Camp', '233209'), ('Hoover Campground', '233693'), ('Humbug Campground', '251713'), ('Olallie Lake Guard Station Cabin', '233305'), ('Riverside At Detroit (Willamette National Forest, Or)', '233258'), ('Santiam Flats Campground', '251616'), ('Southshore At Detroit Lake', '255135'), ('Whispering Falls Campground', '251470')]  
