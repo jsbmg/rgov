5116a9ad723cc5ac83b804428fcd92920f5e3f2f
Jordan Sweet <hello@jordandsweet.com>
2021-12-12 11:47:57 -0800

    Formatted

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,19 +1,41 @@
 import pytest
-
 from rgov.search import search
+
 
 def test_search():
     with pytest.raises(ValueError):
         for x in search("laguna", 1):
-            pass 
+            pass
     for x in search(["laguna"], descriptions=False):
-        assert x == ('Laguna', '232279') 
+        assert x == ("Laguna", "232279")
     dt_search = []
     for x in search(["Detroit"], descriptions=True):
         dt_search.append(x)
 
-    assert dt_search == [('Breitenbush Campground', '233301'), ('Cove Creek', '233694'), ('Fox Creek Group Camp', '233209'), ('Hoover Campground', '233693'), ('Humbug Campground', '251713'), ('Olallie Lake Guard Station Cabin', '233305'), ('Riverside At Detroit (Willamette National Forest, Or)', '233258'), ('Santiam Flats Campground', '251616'), ('Southshore At Detroit Lake', '255135'), ('Whispering Falls Campground', '251470')]  
+    assert dt_search == [
+        ("Breitenbush Campground", "233301"),
+        ("Cove Creek", "233694"),
+        ("Fox Creek Group Camp", "233209"),
+        ("Hoover Campground", "233693"),
+        ("Humbug Campground", "251713"),
+        ("Olallie Lake Guard Station Cabin", "233305"),
+        ("Riverside At Detroit (Willamette National Forest, Or)", "233258"),
+        ("Santiam Flats Campground", "251616"),
+        ("Southshore At Detroit Lake", "255135"),
+        ("Whispering Falls Campground", "251470"),
+    ]
     dt_uncaps_search = []
     for x in search(["detroit"], descriptions=True):
         dt_uncaps_search.append(x)
-    assert dt_uncaps_search == [('Breitenbush Campground', '233301'), ('Cove Creek', '233694'), ('Fox Creek Group Camp', '233209'), ('Hoover Campground', '233693'), ('Humbug Campground', '251713'), ('Olallie Lake Guard Station Cabin', '233305'), ('Riverside At Detroit (Willamette National Forest, Or)', '233258'), ('Santiam Flats Campground', '251616'), ('Southshore At Detroit Lake', '255135'), ('Whispering Falls Campground', '251470')]  
+    assert dt_uncaps_search == [
+        ("Breitenbush Campground", "233301"),
+        ("Cove Creek", "233694"),
+        ("Fox Creek Group Camp", "233209"),
+        ("Hoover Campground", "233693"),
+        ("Humbug Campground", "251713"),
+        ("Olallie Lake Guard Station Cabin", "233305"),
+        ("Riverside At Detroit (Willamette National Forest, Or)", "233258"),
+        ("Santiam Flats Campground", "251616"),
+        ("Southshore At Detroit Lake", "255135"),
+        ("Whispering Falls Campground", "251470"),
+    ]
